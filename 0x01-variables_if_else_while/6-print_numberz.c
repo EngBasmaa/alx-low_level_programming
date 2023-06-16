7e7736abccdbc127a34e9bea98c2068e0b336695
EngBasmaa <basmaelmasry263@gmail.com>
2023-06-16 16:34:45 -0700

    ii

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,10 @@
 
 /**
  * main - main block.
- * Description: Print all numbers of base 10, starting from 0.
- * You are not allowed to use any variable of type char.
+ * Description:You are not allowed to use any variable of type char.
  * You can only use `putchar` to print to console.
  * You can only use `putchar` twice.
- * Rreturn: 0  (success)
+ * Return: 0 (success)
 */
 
 int main(void)
