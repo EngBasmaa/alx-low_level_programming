db7a8e28e5adf5b8e58d7334a481d85610ac1bcd
EngBasmaa <basmaelmasry263@gmail.com>
2023-06-23 15:45:32 -0700

    t

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int num = 0;
+	int ch;
 
-	do {
-		(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
 	_putchar('\n');
 }
