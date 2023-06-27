bdcd015b0e24b22dce1a3e3c134391a0afd5b962
EngBasmaa <basmaelmasry263@gmail.com>
2023-06-27 13:10:45 -0700

    s

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 
 /**
  * puts_half - print half of a string
- *@str: a string
+ * @str: a string
  * Return: Always 0.
  */
 
@@ -11,10 +11,12 @@ void puts_half(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0';)
+	{
 		i++;
 	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar(str[i]);
+	}
 }
