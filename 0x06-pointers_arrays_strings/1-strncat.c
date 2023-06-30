3ebb7ce7d45fd3f901b6c6d2dff0c18d213000d7
EngBasmaa <basmaelmasry263@gmail.com>
2023-06-29 18:20:21 -0700

    d

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - concat function srtings
+ * @dest: destination parameter pointer to a char
+ * @src: source parameter pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+
+	while (dest[x] != '\0')
+		x++;
+
+	while (src[y] != '\0' && y < n)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+	dest[x] = '\0';
+
+	return (dest);
+}
