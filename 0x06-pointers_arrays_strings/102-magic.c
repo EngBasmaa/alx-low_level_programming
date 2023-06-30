6e6ec8f3e180dd4d6fceefdba3eac0da982c566e
EngBasmaa <basmaelmasry263@gmail.com>
2023-06-30 16:47:39 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main - function for printing value updated of the pointer
+ * @p: pointer
+ * This is the main function of the program.
+ * It performs on a,n,*p operations and returns an exit status
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * Return: always 0 (success)
+*/
 int main(void)
 {
 	int n;
@@ -15,9 +24,9 @@ int main(void)
  * - you are not allowed to modify p
  * - only one statement
  * - you are not allowed to code anything else than this line of code
- */
+*/
 	*(p + 5) = 98;
-/* ...so that this prints 98\n */
+/* to print: 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
