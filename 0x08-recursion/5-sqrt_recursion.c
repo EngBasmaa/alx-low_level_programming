deb56b77591334c607a359825b5aa5c98ffbdf5a
EngBasmaa <basmaelmasry263@gmail.com>
2023-07-05 19:56:49 -0700

    xx

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,29 @@
  * @i: number squared
  * Return: -1 when n is not natural number
 */
-
-int func_square_recursion(int n, int i)
+int power_operation(int n, int c)
 {
-	if (i * i > n)
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 		return (-1);
-
-	if (i * i == n)
-		return (i);
-	return (func_square_recursion(n, i + 1));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
