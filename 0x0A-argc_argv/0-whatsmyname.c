1ba3ed5283a5785d1620fc46f419b849c8b604e2
EngBasmaa <basmaelmasry263@gmail.com>
2023-07-07 18:21:45 -0700

    sA

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  prints its name, followed by a new line.
+ * @argv: the list of arguments
+ * @argc: integer 
+ * Return: 0  (success)
+*/
+
+int main(int argc, char cinst *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
