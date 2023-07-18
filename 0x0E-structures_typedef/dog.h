7970f7d136755ae00217a7bea3791814a32a02ec
basma <basmaelmasry263@gmail.com>
2023-07-18 04:30:36 +0300

    g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * main - define a dog structure
+ * struct - define a dog structure
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner
@@ -17,7 +17,6 @@ struct dog
 };
 
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
