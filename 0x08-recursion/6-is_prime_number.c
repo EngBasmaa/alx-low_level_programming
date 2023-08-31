ea0d7b3248a61ddc511d1f6a7b029372f54cbd7d
EngBasma <basmaelmasry263@gmail.com>
2023-08-31 04:58:34 -0700

    k

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,6 +29,5 @@ int testing_prime(int n, int d)
 		if (d == 1)
 			return (1);
 		if (n % d == 0)
-                        return (0);
-		return (testing_prime(n, d - 1));
+			return (testing_prime(n, d - 1));
 }
