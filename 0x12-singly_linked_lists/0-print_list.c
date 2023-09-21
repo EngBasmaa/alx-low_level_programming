34f8e3fc93cfac5e73de9d60cdffbc2e2a2aaa11
EngBasmaa <basmaelmasry263@gmail.com>
2023-09-21 16:58:31 -0700

    h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,23 +4,6 @@
 #include <stdio.h>
 
 /**
- * length_st - to measure the string length
- * @c: input var
- * Return:  length of an string
-*/
-
-int length_st(char *c)
-{
-	size_t lst_size = 0;
-
-	if (c)
-		while (*c++)
-			lst_size++;
-	else
-		return (0);
-	return (lst_size);
-}
-/**
  * print_list - print elements of list_t list
  * @h: a pointer to list
  * Return: number of nodes in linked list
