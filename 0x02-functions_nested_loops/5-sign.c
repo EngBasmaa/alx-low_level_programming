d8339fdf094958eb64ae5bde8fa098342f11a55e
EngBasmaa <basmaelmasry263@gmail.com>
2023-09-23 05:57:56 -0700

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -15,14 +15,13 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
-	}
+	{
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
 	}
 }
-
