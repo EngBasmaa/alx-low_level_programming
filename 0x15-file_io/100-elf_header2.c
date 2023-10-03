edca89fb194153587c92ceef77485094c40365f1
EngBasmaa <basmaelmasry263@gmail.com>
2023-10-02 18:34:01 -0700

    l

--- 0x15-file_io/100-elf_header2.c
+++ 0x15-file_io/100-elf_header2.c
@@ -3,54 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 void printElf_abi(unsigned char *e_ident);
-void printElf_osabi(unsigned char *e_ident);
 void printElf_type(unsigned int e_type, unsigned char *e_ident);
 void printElf_entry(unsigned long int e_entry, unsigned char *e_ident);
-void closeElf(int elf);
-/**
- * printElf_osabi - Prints the OS/ABI of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
- */
-void printEls_osabi(unsigned char *e_ident)
-{
-	printf("  OS/ABI:     ");
-
-	switch (e_ident[EI_OSABI])
-	{
-	case ELFOSABI_NONE:
-		printf("UNIX - System V\n");
-		break;
-	case ELFOSABI_HPUX:
-		printf("UNIX - HP-UX\n");
-		break;
-	case ELFOSABI_NETBSD:
-		printf("UNIX - NetBSD\n");
-		break;
-	case ELFOSABI_LINUX:
-		printf("UNIX - Linux\n");
-		break;
-	case ELFOSABI_SOLARIS:
-		printf("UNIX - Solaris\n");
-		break;
-	case ELFOSABI_IRIX:
-		printf("UNIX - IRIX\n");
-		break;
-	case ELFOSABI_FREEBSD:
-		printf("UNIX - FreeBSD\n");
-		break;
-	case ELFOSABI_TRU64:
-		printf("UNIX - TRU64\n");
-		break;
-	case ELFOSABI_ARM:
-		printf("ARM\n");
-		break;
-	case ELFOSABI_STANDALONE:
-		printf("Standalone App\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
-	}
-}
 
 /**
  * printElf_abi - Prints the ABI version of an ELF header.
@@ -117,20 +71,4 @@ void printElf_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 	else
 		printf("%#lx\n", e_entry);
-}
-
-/**
- * closeElf_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- *
- * Description: If the file cannot be closed - exit code 98.
- */
-void closeElf_elf(int elf)
-{
-	if (close(elf) == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", elf);
-		exit(98);
-	}
 }
